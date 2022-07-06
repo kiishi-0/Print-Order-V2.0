868a5300c2532598e84b5d0a5d866b9bb20548cb
kiishi-0 <kiishioluwa@gmail.com>
2022-07-06 21:13:00 +0100

    changed the designs of the overlay

--- src/Components/Overlay/Overlay.jsx
+++ src/Components/Overlay/Overlay.jsx
@@ -11,26 +11,32 @@ const OverlayDiv = styled.div`
     width: 390px;
     height: 200px;
     background: #585858;
+    padding: 0 20px;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     align-items: center;
     justify-content: center;
-    border-radius: 20px;
+    // border-radius: 20px;
     flex-direction: column;
     img{
         width: 30px;
         cursor: pointer;
     }  
-
+    @media(max-width: 422px){
+        width: calc(100% - 25px);
+        height: 220px;
+    }
 `
 const ImageDiv = styled.div`
     display: flex;
     width: 100%;
     padding: 20px 20px 0 0;
     // background: red;
+    position: absolute;
     justify-content: flex-end;
-    position: relative;
+    top: 0;
+    // position: relative;
     justify-self: flex-start;
     
 `
@@ -38,12 +44,12 @@ const Message = styled.p`
     flex: 3;
     width: 100%;
     color: #fff;
-    font-size: 25px;
-    padding-top: 10%;
-    // display: flex;
-    // align-items: center;
-    // justify-content: center;
+    font-size: 20px;
+    // padding-top: 10%;
     text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 export default function Overlay(props) {
         
@@ -59,7 +65,7 @@ export default function Overlay(props) {
             <ImageDiv>
                 <img src={close2} alt="close" onClick={handleCloseDisplay}/>
             </ImageDiv>
-            <Message>Order Submitted</Message>
+            <Message>Order Submitted, we will communicate with you soon.</Message>
         </OverlayDiv>
     </div>
   )
